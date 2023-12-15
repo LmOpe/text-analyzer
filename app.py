6a3795915d05791aeed6c1e8cbdb1af964e3f129
LmOpe <lawalmuhammedope@gmail.com>
2023-12-15 15:54:02 +0100

    Added Vercel.json file

--- app.py
+++ app.py
@@ -67,9 +67,3 @@ def post():
             'num_of_char': len(chars)
         }
         return redirect(url_for('result', **data))
-
-if __name__ == '__main__':
-    # for deployment
-    # to make it work for both production and development
-    port = int(os.environ.get("PORT", 5000))
-    app.run(debug=True, host='0.0.0.0', port=port)
